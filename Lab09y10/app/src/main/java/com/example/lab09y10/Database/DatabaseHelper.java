8a5be633a575d4e900742b1cf8cb0469f2d12d82
Carlos Obando Avendaña <49579027+CarlosAvendana@users.noreply.github.com>
2020-06-05 14:01:30 -0600

    database upd

--- Lab09y10/app/src/main/java/com/example/lab09y10/Database/DatabaseHelper.java
+++ Lab09y10/app/src/main/java/com/example/lab09y10/Database/DatabaseHelper.java
@@ -8,15 +8,16 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+
     public static final String ESTUDIANTE_TABLE = "ESTUDIANTE_TABLE";
-    public static final String COLUMN_ID_CURSO = "id";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_NOMBRE = "_nombre";
-    public static final String COLUMN_APELLIDO = "_apellido";
-    public static final String COLUMN_ANIOS = "_anios";
-    public static final String COLUMN_DESCRIPCION_CURSO = "descripcion";
-    public static final String COLUMN_CREDITOS_CURSO = "creditos";
+    public static final String ESTUDIANTE_ID = "ESTUDIANTE_ID";
+    public static final String ESTUDIANTE_NOMBRE = "ESTUDIANTE_NOMBRE";
+    public static final String ESTUDIANTE_APELLIDO = "ESTUDIANTE_APELLIDO";
+    public static final String ESTUDIANTE_ANIO = "ESTUDIANTE_ANIO";
     public static final String CURSO_TABLE = "CURSO_TABLE";
+    public static final String CURSO_ID = "CURSO_ID";
+    public static final String CURSO_DESCRIPCION = "CURSO_DESCRIPCION";
+    public static final String CURSO_CREDITOS = "CURSO_CREDITOS";
     public static final String MATRICULA_TABLE = "MATRICULA_TABLE";
 
     public DatabaseHelper(@Nullable Context context) {
@@ -26,13 +27,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //this is called the first time, create the tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE " + ESTUDIANTE_TABLE + "( " + COLUMN_ID + " TEXT PRIMARY KEY, " + COLUMN_NOMBRE + " TEXT, " + COLUMN_APELLIDO + " TEXT, " + COLUMN_ANIOS + " INTEGER); " +
-                "CREATE TABLE " + CURSO_TABLE + " (" + COLUMN_ID_CURSO + " TEXT PRIMARY KEY, " + COLUMN_DESCRIPCION_CURSO + " TEXT, " + COLUMN_CREDITOS_CURSO + " INTEGER);" +
-                "CREATE TABLE " + MATRICULA_TABLE + "(estudiante_id INTEGER not null," +
-                "curso_id INTEGER not null," +
-                "constraint MATRICULA_TABLE_PK primary key(estudiante_id,curso_id)" +
-                "constraint MATRICULA_TABLE_ESTUDIANTE_FK foreig key (estudiante_id) references " + ESTUDIANTE_TABLE + "(" + COLUMN_ID + ")," +
-                "constraint MATRICULA_TABLE_CURSO_FK foreig key (curso_id) references " + CURSO_TABLE + "(" + COLUMN_ID_CURSO + "));";
+        String createTableStatement = "CREATE TABLE " + ESTUDIANTE_TABLE + " ( " + ESTUDIANTE_ID + " TEXT PRIMARY KEY, " + ESTUDIANTE_NOMBRE + " TEXT, " + ESTUDIANTE_APELLIDO + " TEXT, " + ESTUDIANTE_ANIO + " INTEGER);" +
+                "CREATE TABLE " + CURSO_TABLE + " ( " + CURSO_ID + " TEXT PRIMARY KEY, " + CURSO_DESCRIPCION + " TEXT, " + CURSO_CREDITOS + " INTEGER);" +
+                "CREATE TABLE " + MATRICULA_TABLE + " (" +
+                "    " + ESTUDIANTE_ID + " TEXT NOT NULL," +
+                "    " + CURSO_ID + " TEXT NOT NULL," +
+                "    CONSTRAINT " + MATRICULA_TABLE + "_PK PRIMARY KEY(" + ESTUDIANTE_ID + "," + CURSO_ID + ")," +
+                "    CONSTRAINT " + MATRICULA_TABLE + "_ESTUDIANTE_FK FOREIGN  KEY (" + ESTUDIANTE_ID + ") REFERENCES  " + ESTUDIANTE_TABLE + " ( " + ESTUDIANTE_ID + " )" +
+                "    CONSTRAINT " + MATRICULA_TABLE + "_CURSO_FK FOREIGN  KEY (" + CURSO_ID + ") REFERENCES  " + CURSO_TABLE + " (" + CURSO_ID + ")" +
+                "    );";
         db.execSQL(createTableStatement);
     }
 
